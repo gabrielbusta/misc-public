77a1c33baf56520a2e6b61f2464cb72f0cf3aafd
Gabriel Bustamante <gabrielbusta@gmail.com>
2023-07-27 12:07:54 -0500

    Create docker-misc.sh

---
+++ docker-misc.sh
@@ -0,0 +1,5 @@
+unzstd docker-image-debian11-repackage.tar.zst
+docker load --input docker-image-debian11-repackage.tar
+export SRC="/Users/gbustamante/workspace/clean/mozilla-central"
+export TARGET="/builds/worker/checkouts/gecko"
+docker run -it --mount src=$SRC,target=$TARGET,type=bind --name debian11-repackage-$(date +%s) --platform linux/amd64 debian11-repackage
