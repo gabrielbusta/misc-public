a95037855db13cd309dea420821602a138373829
Gabriel Bustamante <gabrielbusta@gmail.com>
2023-07-27 15:17:11 -0500

    Update docker-misc.sh

--- docker-misc.sh
+++ docker-misc.sh
@@ -4,7 +4,11 @@ export SRC="/Users/gbustamante/workspace/clean/mozilla-central"
 export TARGET="/builds/worker/checkouts/gecko"
 export EXTRA_MOZHARNESS_CONFIG="{\"objdir\": \"obj-build\", \"repackage_config\": [{\"args\": [\"deb\", \"--arch\", \"x86_64\", \"--templates\", \"browser/installer/linux/app/debian\", \"--version\", \"117.0a1\", \"--build-number\", \"1\", \"--release-product\", \"None\", \"--release-type\", \"nightly\"], \"inputs\": {\"input\": \"target.tar.bz2\"}, \"output\": \"target.deb\"}]}"
 docker run \
--it --mount src=$SRC,target=$TARGET,type=bind --name debian11-repackage-$(date +%s) \
+-it --mount src=$SRC,target=$TARGET,type=bind \
+--name debian11-repackage-$(date +%s) \
 --platform linux/amd64 \
---env GECKO_PATH=$TARGET --env MOZ_FETCHES_DIR="/builds/worker/fetches" --env UPLOAD_DIR="/builds/worker/artifacts" \
+--env GECKO_PATH=$TARGET \
+--env MOZ_FETCHES_DIR="/builds/worker/fetches" \
+--env UPLOAD_DIR="/builds/worker/artifacts" \
+--env EXTRA_MOZHARNESS_CONFIG=$EXTRA_MOZHARNESS_CONFIG \
 debian11-repackage
