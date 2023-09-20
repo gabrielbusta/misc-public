f9bf680d1b35494b6a2a9dd32ec5f94f9732e2d6
Gabriel Bustamante <gabrielbusta@gmail.com>
2023-09-20 13:53:02 -0500

    Update docker-misc.sh

--- docker-misc.sh
+++ docker-misc.sh
@@ -2,7 +2,10 @@ export DOCKER_IMAGE="debian12-repackage"
 export CONTAINER_NAME=$DOCKER_IMAGE-$(date +%s)
 export SRC=$(pwd)
 export TARGET="/builds/worker/checkouts/gecko"
-export EXTRA_MOZHARNESS_CONFIG="{\"objdir\": \"obj-build\", \"repackage_config\": [{\"args\": [\"deb\", \"--arch\", \"x86_64\", \"--templates\", \"browser/installer/linux/app/debian\", \"--version\", \"117.0a1\", \"--build-number\", \"1\", \"--release-product\", \"None\", \"--release-type\", \"nightly\"], \"inputs\": {\"input\": \"target.tar.bz2\"}, \"output\": \"target.deb\"}]}"
+# app
+# export EXTRA_MOZHARNESS_CONFIG="{\"objdir\": \"obj-build\", \"repackage_config\": [{\"args\": [\"deb\", \"--arch\", \"x86_64\", \"--templates\", \"browser/installer/linux/app/debian\", \"--version\", \"117.0a1\", \"--build-number\", \"1\", \"--release-product\", \"None\", \"--release-type\", \"nightly\"], \"inputs\": {\"input\": \"target.tar.bz2\"}, \"output\": \"target.deb\"}]}"
+#l10n
+export EXTRA_MOZHARNESS_CONFIG="{\"objdir\": \"obj-build\", \"repackage_config\": [{\"args\": [\"deb-l10n\", \"--version\", \"119.0a1\", \"--build-number\", \"1\", \"--templates\", \"browser/installer/linux/langpack/debian\"], \"inputs\": {\"input-tar-file\": \"target.tar.bz2\", \"input-xpi-file\": \"target.langpack.xpi\"}, \"output\": \"target.langpack.deb\"}]}"
 
 unzstd "$DOCKER_IMAGE.tar.zst"
 
