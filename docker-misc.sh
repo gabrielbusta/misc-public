3ae7c3d30c6c131ffc3d6439d5da6e35993da4d9
Gabriel Bustamante <gabrielbusta@gmail.com>
2023-09-20 13:54:52 -0500

    Update docker-misc.sh

--- docker-misc.sh
+++ docker-misc.sh
@@ -1,4 +1,8 @@
 export DOCKER_IMAGE="debian12-repackage"
+
+unzstd "$DOCKER_IMAGE.tar.zst"
+docker load --input "$DOCKER_IMAGE.tar"
+
 export CONTAINER_NAME=$DOCKER_IMAGE-$(date +%s)
 export SRC=$(pwd)
 export TARGET="/builds/worker/checkouts/gecko"
@@ -6,10 +10,6 @@ export TARGET="/builds/worker/checkouts/gecko"
 # export EXTRA_MOZHARNESS_CONFIG="{\"objdir\": \"obj-build\", \"repackage_config\": [{\"args\": [\"deb\", \"--arch\", \"x86_64\", \"--templates\", \"browser/installer/linux/app/debian\", \"--version\", \"117.0a1\", \"--build-number\", \"1\", \"--release-product\", \"None\", \"--release-type\", \"nightly\"], \"inputs\": {\"input\": \"target.tar.bz2\"}, \"output\": \"target.deb\"}]}"
 #l10n
 export EXTRA_MOZHARNESS_CONFIG="{\"objdir\": \"obj-build\", \"repackage_config\": [{\"args\": [\"deb-l10n\", \"--version\", \"119.0a1\", \"--build-number\", \"1\", \"--templates\", \"browser/installer/linux/langpack/debian\"], \"inputs\": {\"input-tar-file\": \"target.tar.bz2\", \"input-xpi-file\": \"target.langpack.xpi\"}, \"output\": \"target.langpack.deb\"}]}"
-
-unzstd "$DOCKER_IMAGE.tar.zst"
-
-docker load --input "$DOCKER_IMAGE.tar"
 
 docker run \
 -it --mount src=$SRC,target=$TARGET,type=bind \
