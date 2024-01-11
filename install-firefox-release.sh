21e28dca22abc9ede4b646d282b77b862e699797
Gabriel Bustamante <gabrielbusta@gmail.com>
2024-01-11 15:28:27 -0600

    Update install-firefox-release.sh

--- install-firefox-release.sh
+++ install-firefox-release.sh
@@ -16,4 +16,4 @@ Pin: origin packages.mozilla.org
 Pin-Priority: 1005
 ' | sudo tee /etc/apt/preferences.d/mozilla-firefox
 
-sudo snap remove firefox && sudo apt-get update && sudo apt-get install firefox
+sudo apt-get update && sudo apt-get install firefox
