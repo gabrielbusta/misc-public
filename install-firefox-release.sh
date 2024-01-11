2bcca71e5ebefa8d2b96f371ac4e41f8c8b362c1
Gabriel Bustamante <gabrielbusta@gmail.com>
2024-01-11 13:00:48 -0600

    Create install-firefox-release.sh

---
+++ install-firefox-release.sh
@@ -0,0 +1,19 @@
+# Create a directory to store APT repository keys if it doesn't exist:
+sudo install -d -m 0755 /etc/apt/keyrings
+
+# Import the Mozilla APT repository signing key:
+wget -q https://packages.mozilla.org/apt/repo-signing-key.gpg -O- | sudo tee /etc/apt/keyrings/packages.mozilla.org.asc > /dev/null
+
+# The fingerprint should be 35BAA0B33E9EB396F59CA838C0BA5CE6DC6315A3
+gpg -n -q --import --import-options import-show /etc/apt/keyrings/packages.mozilla.org.asc | awk '/pub/{getline; gsub(/^ +| +$/,""); print "\n"$0"\n"}'
+
+# Next, add the Mozilla APT repository to your sources list:
+echo "deb [signed-by=/etc/apt/keyrings/packages.mozilla.org.asc] https://packages.mozilla.org/apt mozilla main" | sudo tee -a /etc/apt/sources.list.d/mozilla.list > /dev/null
+
+echo '
+Package: firefox
+Pin: origin packages.mozilla.org
+Pin-Priority: 1000
+' | sudo tee /etc/apt/preferences.d/mozilla-firefox
+
+sudo snap remove firefox && sudo apt-get update && sudo apt-get install firefox
