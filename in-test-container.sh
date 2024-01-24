30ab8e47961084ac91c89ac5fbba1020486ace22
Gabriel Bustamante <gabrielbusta@gmail.com>
2024-01-24 14:45:05 -0600

    Update in-test-container.sh

--- in-test-container.sh
+++ in-test-container.sh
@@ -1,1 +1,2 @@
 python -I -m pip install -r /builds/worker/checkouts/vcs/requirements/test.txt --no-deps
+py.test -n auto --cov-config=tox.ini --cov-append --cov=auslib --cov-report term-missing tests
