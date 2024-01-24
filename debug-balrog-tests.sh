33727ff9f34f8bb3663c4dc54d73952d9230badd
Gabriel Bustamante <gabrielbusta@gmail.com>
2024-01-24 14:18:00 -0600

    Create debug-balrog-tests.sh

---
+++ debug-balrog-tests.sh
@@ -0,0 +1,14 @@
+export DOCKER_IMAGE_URL="https://firefox-ci-tc.services.mozilla.com/api/queue/v1/task/FIFCGTSoSM-Mzg7crJjYdQ/runs/0/artifacts/public%2Fimage.tar.zst"
+export DOCKER_IMAGE="python3.11"
+export CONTAINER_NAME=$DOCKER_IMAGE-$(date +%s)
+export TARGET="/builds/worker/checkouts/vcs"
+export SRC=$(pwd)
+
+unzstd "$DOCKER_IMAGE.tar.zst"
+docker load --input "$DOCKER_IMAGE.tar"
+
+docker run \
+-it --mount src=$SRC,target=$TARGET,type=bind \
+--name $CONTAINER_NAME \
+--platform linux/amd64 \
+$DOCKER_IMAGE
