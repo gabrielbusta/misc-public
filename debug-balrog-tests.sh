0e8980f6d06f9f3282b9e161fe0b8f524d9881c1
Gabriel Bustamante <gabrielbusta@gmail.com>
2024-01-24 14:32:44 -0600

    Update debug-balrog-tests.sh

--- debug-balrog-tests.sh
+++ debug-balrog-tests.sh
@@ -3,22 +3,23 @@
 set -x
 set -euo pipefail
 
-export DOCKER_IMAGE_URL="https://firefox-ci-tc.services.mozilla.com/api/queue/v1/task/FIFCGTSoSM-Mzg7crJjYdQ/runs/0/artifacts/public%2Fimage.tar.zst"
-export DOCKER_IMAGE_ZST_FILE="image.tar.zst"
 export DOCKER_IMAGE="python3.11"
+export DOCKER_IMAGE_URL="https://firefox-ci-tc.services.mozilla.com/api/queue/v1/task/FIFCGTSoSM-Mzg7crJjYdQ/runs/0/artifacts/public%2Fimage.tar.zst"
 export CONTAINER_NAME=$DOCKER_IMAGE-$(date +%s)
 export TARGET="/builds/worker/checkouts/vcs"
 export SRC=$(pwd)
 
 echo "Downloading compressed $DOCKER_IMAGE from $DOCKER_IMAGE_URL"
-wget -O $DOCKER_IMAGE_ZST_FILE $DOCKER_IMAGE_URL
+wget -O "$DOCKER_IMAGE.tar.zst" $DOCKER_IMAGE_URL
 
-echo "Uncompressing $DOCKER_IMAGE_ZST_FILE"
+echo "Uncompressing $DOCKER_IMAGE.tar.zst"
 unzstd "$DOCKER_IMAGE.tar.zst"
-echo "Cleaning up $DOCKER_IMAGE_ZST_FILE"
-rm $DOCKER_IMAGE_ZST_FILE
+echo "Cleaning up $DOCKER_IMAGE.tar.zst"
+rm "$DOCKER_IMAGE.tar.zst"
 
+echo "Loading $DOCKER_IMAGE.tar"
 docker load --input "$DOCKER_IMAGE.tar"
+echo "Cleaning up $DOCKER_IMAGE.tar"
 
 docker run \
 -it --mount src=$SRC,target=$TARGET,type=bind \
