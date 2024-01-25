b75ec0bd78734b0bb3c57b9c7181d23a6de78213
Gabriel Bustamante <gabrielbusta@gmail.com>
2024-01-25 11:29:21 -0600

    Update in-test-container.sh

--- in-test-container.sh
+++ in-test-container.sh
@@ -1,5 +1,13 @@
+# Update the PYTHONPATH to include Balrog's source code (so the tests can import Balrog modules.)
 export PYTHONPATH="${PYTHONPATH}:/builds/worker/checkouts/vcs/src"
+# Install the test's dependencies, you only have to do this when the container boots up.
 python -I -m pip install -r /builds/worker/checkouts/vcs/requirements/test.txt --no-deps
+
+# To run all the tests:
 py.test --pdb -n auto --cov-config=tox.ini --cov-append --cov=auslib --cov-report term-missing tests
 
+# Ex. of running a specific test:
 py.test --pdb -n auto --cov-config=tox.ini --cov-append --cov=auslib --cov-report term-missing tests/test_db.py::TestPermissions::testGrantPermissions
+
+# The excution will stop at the first raised Exception or breakpoint
+# and drop you into the builtin Python debugger https://docs.python.org/3/library/pdb.html
