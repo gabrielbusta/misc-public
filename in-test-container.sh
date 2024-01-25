07f66e59f98fbd92551f0377a217888ecd32d573
Gabriel Bustamante <gabrielbusta@gmail.com>
2024-01-25 10:32:58 -0600

    Update in-test-container.sh

--- in-test-container.sh
+++ in-test-container.sh
@@ -1,3 +1,5 @@
 export PYTHONPATH="${PYTHONPATH}:/builds/worker/checkouts/vcs/src"
 python -I -m pip install -r /builds/worker/checkouts/vcs/requirements/test.txt --no-deps
 py.test --pdb -n auto --cov-config=tox.ini --cov-append --cov=auslib --cov-report term-missing tests
+
+py.test --pdb -n auto --cov-config=tox.ini --cov-append --cov=auslib --cov-report term-missing tests/test_db.py::testGrantPermissions
