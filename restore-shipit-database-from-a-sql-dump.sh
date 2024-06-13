3e8db230d60d9ae77dba9921687b9c25eeab75b5
Gabriel Bustamante <gabrielbusta@gmail.com>
2024-06-13 11:35:31 -0500

    Create restore-shipit-database-from-a-sql-dump.sh

---
+++ restore-shipit-database-from-a-sql-dump.sh
@@ -0,0 +1,34 @@
+# Restore shipit database from a sql dump
+
+# STOP the non-database containers
+
+CONTAINER="shipit-db-1"
+DB_DUMP="shipitdb_2024-05-15.sql"
+docker cp "$DB_DUMP" "$CONTAINER":/tmp/dumpfile.sql
+docker exec -u root -it "$CONTAINER" /bin/bash
+# In the container
+psql -U shipituser -h localhost -d postgres
+# In the psql shell
+DROP DATABASE IF EXISTS shipitdb;
+CREATE DATABASE shipitdb;
+\q
+# In the container
+psql -U shipituser -d shipitdb -f /tmp/dumpfile.sql
+
+# psql -U shipituser -h localhost -d postgres
+# CREATE ROLE cloudsqladmin LOGIN;
+# CREATE ROLE cloudsqlsuperuser LOGIN;
+# CREATE ROLE shipitpublicprod LOGIN;
+# \q
+
+psql -U shipituser -h localhost -d shipitdb
+GRANT USAGE ON SCHEMA public TO shipituserreadonly;
+GRANT SELECT ON ALL TABLES IN SCHEMA public TO shipituserreadonly;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO shipituserreadonly;
+
+\q
+
+# Start a nice shell for tinkering in the shipit-api container
+docker exec -it shipit-api-1 /bin/bash
+export FLASK_APP="shipit_api.admin.flask:app"
+poetry run flask shell
