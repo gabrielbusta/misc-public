187ebe1291862ee4971e47fc6c8f562fcf05ff00
Gabriel Bustamante <gabrielbusta@gmail.com>
2025-01-30 16:15:33 -0600

    Update misc-public.py

--- misc-public.py
+++ misc-public.py
@@ -46,6 +46,36 @@ def load_firefox_ci_task_graph(task_id, task_run=0):
     return task_graph, label_to_task_id
 
 
+def rerun_failed_tasks_in_task_group(task_group_id, dry_run=True):
+    import taskcluster
+    from pprint import pp
+
+    auth = taskcluster.Auth(taskcluster.optionsFromEnvironment())
+    queue = taskcluster.Queue(auth.options)
+    task_group = queue.listTaskGroup(task_group_id)
+    failed_tasks_in_task_group = [
+        task for task in task_group["tasks"] if task["status"]["state"] == "failed"
+    ]
+    print(f"Found {len(failed_tasks_in_task_group)} failed tasks in {task_group_id}.")
+    failed_tasks_in_task_group_names = {
+        task["task"]["metadata"]["name"]: task["status"]["taskId"]
+        for task in failed_tasks_in_task_group
+    }
+    pp(failed_tasks_in_task_group_names)
+    if dry_run:
+        print("THIS IS A DRY RUN!")
+    for task_name, task_id in failed_tasks_in_task_group_names.items():
+        print(f"Rerunning {task_name} ({task_id})")
+        if dry_run:
+            print(f'Calling queue.rerunTask("{task_id}")')
+        else:
+            response = queue.rerunTask(task_id)
+            run_id = response["status"]["runs"][-1]["runId"]
+            run_state = response["status"]["runs"][-1]["state"]
+            print(f"Run {run_id} for {task_name} is {run_state}.")
+    print(f'DONE SUBMITTING FAILED TASK RERUNS FOR "{task_group_id}"')
+
+
 def open_task(task_id):
     import webbrowser
     webbrowser.open(f"https://firefox-ci-tc.services.mozilla.com/tasks/{task_id}", new=2)
