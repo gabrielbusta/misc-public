ee223ab753e0118a3bbfc83ed2603a6a6180f9f9
Gabriel Bustamante <gabrielbusta@gmail.com>
2025-02-14 14:06:45 -0600

    Update restore-shipit-database-from-a-sql-dump.sh

--- restore-shipit-database-from-a-sql-dump.sh
+++ restore-shipit-database-from-a-sql-dump.sh
@@ -3,29 +3,30 @@
 # STOP the non-database containers
 
 CONTAINER="shipit-db-1"
-DB_DUMP="shipitdb_2024-05-15.sql"
+DB_DUMP="shipitapi-20250213-212102.sql"
 docker cp "$DB_DUMP" "$CONTAINER":/tmp/dumpfile.sql
 docker exec -u root -it "$CONTAINER" /bin/bash
+
 # In the container
 psql -U shipituser -h localhost -d postgres
 # In the psql shell
 DROP DATABASE IF EXISTS shipitdb;
 CREATE DATABASE shipitdb;
 \q
-# In the container
-psql -U shipituser -d shipitdb -f /tmp/dumpfile.sql
 
-# psql -U shipituser -h localhost -d postgres
-# CREATE ROLE cloudsqladmin LOGIN;
-# CREATE ROLE cloudsqlsuperuser LOGIN;
-# CREATE ROLE shipitpublicprod LOGIN;
-# \q
+psql -U shipituser -h localhost -d postgres
+CREATE ROLE cloudsqladmin LOGIN;
+CREATE ROLE cloudsqlsuperuser LOGIN;
+CREATE ROLE shipitpublicprod LOGIN;
+CREATE ROLE postgres LOGIN;
+\q
+
+psql -U shipituser -d shipitdb -f /tmp/dumpfile.sql
 
 psql -U shipituser -h localhost -d shipitdb
 GRANT USAGE ON SCHEMA public TO shipituserreadonly;
 GRANT SELECT ON ALL TABLES IN SCHEMA public TO shipituserreadonly;
 ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO shipituserreadonly;
-
 \q
 
 # Start a nice shell for tinkering in the shipit-api container
